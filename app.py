1c003d78b8a0d94ddf8af573cb73a9a485824cd9
Darren Henderson <darrenh@ccsj.edu>
2019-09-05 14:18:56 -0500

    Needed to save file.

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ server = app.server
 app.config.suppress_callback_exceptions = True
 
 # Dash CSS
-app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
+# app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
 # Loading screen CSS
-app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/brPBPO.css"})
+# app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/brPBPO.css"})
